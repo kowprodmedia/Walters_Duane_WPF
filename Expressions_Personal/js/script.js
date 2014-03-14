efc1e616f75275ebd76188c61ee59011575bf65a
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-14 00:12:59 -0400

    think I -might- be getting this... a little anyways.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,5 +4,5 @@ var drumsticks = 2;
 var lastsWeeks = 3;
 var year = 12;
 
-var result = drumsticks * lastsWeeks / year;
+var result = year / lastsWeeks * drumsticks + " " + "pairs of drumsticks";
 console.log(result);

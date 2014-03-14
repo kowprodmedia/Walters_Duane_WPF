7b15b6cea803e28f42edbfae89ed37c52c7d0527
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-14 00:50:26 -0400

    still messing around
    I tried to use the “\n” to put a break between the house#/street and
    the city/state/zip but it just kept saying “illegal character” or
    “syntax error”.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,7 +1,36 @@
 //Duane Walters - 03.13.2014 - Industry
 
+
+//How many pixels are in a 1080p movie?
 var width = 1920;
 var height = 1080;
 
 var result = 1920 * 1080 + "px in 1080p film";
 console.log(result);
+
+//Simple form information
+
+var firstName = "Duane";
+var lastName = "Walters";
+
+var nameResult = lastName + ", " + firstName;
+
+var phoneArea = "765";
+var phoneFirstThree = "653";
+var phoneLastFour = "8372";
+
+var phoneNo = "(" + String(phoneArea) + ")" + String(phoneFirstThree) + "-" + String(phoneLastFour);
+
+var houseAddressNumber = "83";
+var streetName = "Elm Street";
+var city = "Kentzville";
+var state = "Florida";
+var zip = "83921";
+
+var addressResult = houseAddressNumber + " " + streetName;
+var cityStateResult = city + ", " + state + " " + zip;
+
+console.log(nameResult);
+console.log(phoneNo);
+console.log(addressResult);
+console.log(cityStateResult);

e911d642dfb8bd56e75454bd80c04c2c740795e5
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-19 21:15:52 -0400

    i hate math, and i hate it in javascript

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,9 +1,10 @@
 //Duane Walters 3.19.2014 - Conditionals Worksheet
 
 
-//Group 1 Celsius to Fahrenheit Converter
+//Group 1 - Celsius to Fahrenheit Converter
 
-var degFahren = Number (prompt ("Enter the degrees in Fahrenheit",32));
+/*
+var degFahren = Number (prompt ("Enter the degrees in Fahrenheit",30));
 var degCent;
 
 degCent = 5/9 * (degFahren - 32);
@@ -14,28 +15,40 @@ console.log(degFahren + "\xB0 Farenheit is " + degCent + "\xB0 celcius");
 if(degCent < 0){
 	document.write("You're freezing water.");
 	console.log("You're freezing water.");
-}
+	}
 
 if(degCent == 100){
 	document.write("You're boiling water.");
 	console.log("You're boiling water.");
-}
+	}
+
+*/
 
 
 
+//Group 2 - Check the Login
 
+/*
+var userName = prompt("Username", "Username");
+var password = prompt("Password", "Password");
 
-//Group 2 Check the Login
-
-var userName = prompt("Username", "blank");
-var password = prompt("Password", " ");
-
-if(userName + password == "James007" || userName + password == "Q007") {
+if(userName + password == "James007" || userName + password == "Q007"){
 	document.write("Hello 007, how are you today?");
 	console.log("Hello 007, how are you today?");
 }else{
 	document.write("Sorry, you have entered the wrong username and/or password. Please try again.");
 	console.log("Sorry, you have entered the wrong username and/or password. Please try again.");
 	}
-	
-	
+*/
+
+
+
+
+//Group 3 - Movie Ticket Price
+
+var regPrice = 12.00;
+var discountPrice = 7.00; //55 and older, or 10 and under
+var discountTime = ("3", "4", "5");
+
+if(regPrice){
+}

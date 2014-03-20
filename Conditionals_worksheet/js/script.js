5ce5a032e2d85b2438245be601b4fd3aea7bfdc6
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-20 18:14:29 -0400

    re-worked groups 1 & 2 based on instructor suggestions

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -4,7 +4,7 @@
 //Group 1 - Celsius to Fahrenheit Converter
 
 
-var degFahren = Number (30);
+var degFahren = Number (213);
 var degCent;
 
 degCent = 5/9 * (degFahren - 32);
@@ -15,11 +15,13 @@ console.log(degFahren + "\xB0 Farenheit is " + degCent + "\xB0 celcius");
 if(degCent < 0){
 	document.write("You're freezing water.<br />");
 	console.log("You're freezing water.");
-	}
-
-if(degCent == 100){
-	document.write("You're boiling water.");
-	console.log("You're boiling water.");
+	}else if (degCent > 100) {
+		document.write("You're boiling water.<br />");
+		console.log("You're boiling water.");
+	}else if (degCent > 0  && degCent <= 100) {
+		document.write("The water is fine.<br />");
+		console.log("The water is fine.");
+		
 	}
 
 
@@ -32,13 +34,24 @@ if(degCent == 100){
 var userName = ("James", "Q");
 var password = ("007", "007");
 
-if(userName + password == "James007" || userName + password == "Q007"){
+if(userName == "James" && password == "007"){
+	document.write("Hello James!<br />");
+	console.log("Hello James!");
+	}else if (userName == "Q" && password == "007") {
+		document.write("Hello Q!<br />");
+		console.log("Hello Q!");
+	}
+
+
+
+
+/*if(userName + password == "James007" || userName + password == "Q007"){
 	document.write("Hello 007, how are you today?");
 	console.log("Hello 007, how are you today?");
 }else{
 	document.write("Sorry, you have entered the wrong username and/or password. Please try again.");
 	console.log("Sorry, you have entered the wrong username and/or password. Please try again.");
-	}
+	}*/
 
 
 

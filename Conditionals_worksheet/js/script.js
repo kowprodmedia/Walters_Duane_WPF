1cbc3a1fd471635876e75cb19f574eeb6f5e5990
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-19 20:08:31 -0400

    working on group 2

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -3,19 +3,33 @@
 
 //Group 1 Celsius to Fahrenheit Converter
 
-var degFahren = Number (prompt ("Enter the degrees in Fahrenheit",32));
+/*var degFahren = Number (prompt ("Enter the degrees in Fahrenheit",32));
 var degCent;
 
 degCent = 5/9 * (degFahren - 32);
 
 document.write(degFahren + "\xB0 Farenheit is " + degCent + "\xB0 celcius<br />");
-console.log(degFahren + "\xB0 Farenheit is " + degCent + "\xB0 celcius<br />");
+console.log(degFahren + "\xB0 Farenheit is " + degCent + "\xB0 celcius");
 
 if(degCent < 0){
 	document.write("You're freezing water.");
 	console.log("You're freezing water.");
-}else{
-	(degCent == 100);
+}
+
+if(degCent == 100){
 	document.write("You're boiling water.");
 	console.log("You're boiling water.");
+}
+
+*/
+
+
+
+//Group 2 Check the Login
+
+var myAge = Number(prompt("Enter Your Age" , 31));
+
+if (myAge >= 0 && myAge <= 10){
+	document.write("myAge is between 0 and 10<br />);
+	console.log("myAge is between 0 and 10);
 }

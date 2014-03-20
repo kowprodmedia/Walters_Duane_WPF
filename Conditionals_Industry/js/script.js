c87db3cc6b8e31063aafde28dbec5b9fe7821a7b
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-20 19:43:56 -0400

    I believe I have my Conditionals_Industry completed!

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,1 +1,17 @@
-//Duane Walters 3.19.2014 - Conditionals Industry
+//Duane Walters 3.20.2014 - Conditionals Industry
+
+
+//Gig Cost vs. Benefit
+
+var cost = 50;
+var ticketPrice = 10;
+var crowd = 3;
+
+if(crowd * ticketPrice < cost) {
+	console.log("No, don't take the gig.");
+	document.write("No, don't take the gig.");
+}else {
+	console.log("Yes, take the gig.");
+	document.write("Yes, take the gig.");
+}
+

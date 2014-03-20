33bb5c7115ca720193012bd068552a3168dcd3f4
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-19 20:56:42 -0400

    was thinking of a way to tie all 3 groups together.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -3,7 +3,7 @@
 
 //Group 1 Celsius to Fahrenheit Converter
 
-/*var degFahren = Number (prompt ("Enter the degrees in Fahrenheit",32));
+var degFahren = Number (prompt ("Enter the degrees in Fahrenheit",32));
 var degCent;
 
 degCent = 5/9 * (degFahren - 32);
@@ -21,7 +21,7 @@ if(degCent == 100){
 	console.log("You're boiling water.");
 }
 
-*/
+
 
 
 

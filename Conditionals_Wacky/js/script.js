adeb15d3a87f28264da7452e427bd92c3033c90d
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-20 22:22:29 -0400

    i'm certain there's a more in-depth way to go about this, but i can't find it right now.
    Thanks so much Mr. Williams, while I don’t feel like I’m quite to where
    I should be, I think I’m well on my, and am actually interested enough
    in this to keep going and playing with it.

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -5,22 +5,22 @@
 
 
 var commercial1 = .25;
-var commercial2 = 1;
-var commercial3 = 1.5;
-var bathroomBrk = 2;
+var commercial2 = 1.5;
+var commercial3 = 1.75;
+var bathroomBrk = 1;
 
 
 
 
 if(commercial1 >= bathroomBrk){
-	console.log("Yes, you can go to the bathroom.");
-	document.write("Yes, you can go to the bathroom.");
+	console.log("Yes, you can go to the bathroom on commercial 1.");
+	document.write("Yes, you can go to the bathroom on commercial 1.");
 		}else if (commercial2 >= bathroomBrk) {
-			console.log("Yes, you can go to the bathroom.");
-			document.write("Yes, you can go to the bathroom.");
+			console.log("Yes, you can go to the bathroom on commercial 2.");
+			document.write("Yes, you can go to the bathroom on commercial 2.");
 		}else if (commercial3 >= bathroomBrk) {
-			console.log("Yes, you can go to the bathroom.");
-			document.write("Yes, you can go to the bathroom.");
+			console.log("Yes, you can go to the bathroom on commercial 3.");
+			document.write("Yes, you can go to the bathroom on commercial 3.");
 		}else {
 			console.log("No, you can't go to the bathroom.");
 			document.write("No, you can't go to the bathroom.");

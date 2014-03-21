9e0cfbdecab2d9c555426acfb6b5ee2e163ca05b
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-20 20:43:33 -0400

    i think i'm done with this
    i was trying to figure out how to get it to also print with the “yes”
    or “no” statement, but nothing i do seems to work.

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -3,9 +3,11 @@
 
 //Gig Cost vs. Benefit
 
-var cost = 50;
+var cost = 100;
 var ticketPrice = 10;
-var crowd = 3;
+var crowd = 11;
+var payment;
+
 
 if(crowd * ticketPrice < cost) {
 	console.log("No, don't take the gig.");

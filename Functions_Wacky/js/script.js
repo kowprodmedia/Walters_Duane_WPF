aa029a3a3a02018f60b2f0632e0a9b346cc30c07
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-27 23:57:12 -0400

    i can't do this.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,4 +1,13 @@
 //Duane Walters - 03.27.2014 - Functions - Wacky... Or -not- so wacky.
 
 
+function simpleMath(){
+var firstNum =prompt ("enter number", "1");
+var secondNum =prompt ("enter number", "2");
+var thirdNum =prompt ("enter number", "3");
+
+var result = firstNum + secondNum + thirdNum;
+	console.log(result);
+	document.write( ("You have "), (result), (" dollars. ") );
+
 

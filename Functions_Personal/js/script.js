165de3031717463f1c247b2510c7d1cb5c03d736
kowprodmedia <duaneawalters@Duanes-Mac-mini.local>
2014-03-27 21:50:32 -0400

    functions - personal is done.
    I hope i did this right. it works, but i have a hard enough time
    remembering how to do it, let alone the names of what i’m doing. in due
    time i suppose?

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,2 +1,22 @@
 //Duane Walters - 03.27.2014 - Functions - Personal
 
+//anonymous function
+
+function sticksUsed(){
+	//setLength = prompt ("How long is your set?", "");
+	var normalUse = prompt ("How many do you go through in an hour?", "");
+	var setLength=prompt("Enter Your Set Length","Length In Hours");
+	var pair = 2;
+	var usedPairs = setLength * normalUse * 2;
+
+
+var x;
+
+//var setLength=prompt("Enter Your Set Length","Length In Hours");
+
+if (setLength!=null)
+  {
+  x="You will use " + usedPairs + " pairs of sticks during your set tonight.";
+  document.getElementById("usedPairs").innerHTML=x;
+  }
+}
